03e87421a3e8fe8d09259ca4214abfad30abfe53
Phi Van Ngoc <ngocketit@gmail.com>
2022-01-26 23:24:42 +0200

    Use emoji

--- examples/simple_progressbar.rs
+++ examples/simple_progressbar.rs
@@ -1,15 +1,19 @@
-use console::style;
-use indicatif::ProgressBar;
+use console::{style, Emoji};
+use indicatif::{HumanDuration, ProgressBar};
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+static SUCCESS: Emoji<'_, '_> = Emoji("✨ ", ":-)");
 
 fn main() {
     let count = 200;
+    let timer = Instant::now();
     let pb = ProgressBar::new(count);
     pb.set_prefix(style("[1/1]").bold().dim().to_string());
     for _ in 0..count {
         pb.inc(1);
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(10));
     }
     pb.finish_and_clear();
+    println!("{} Done in {}", SUCCESS, HumanDuration(timer.elapsed()));
 }
